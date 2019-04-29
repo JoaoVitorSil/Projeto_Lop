f4e16c603c14f0486efeb26caa3ee3b1b34013d8
JoaoVitorSil <48871120+JoaoVitorSil@users.noreply.github.com>
2019-04-29 14:42:16 -0300

    Create Etapa2.js

---
+++ Etapa2.js
@@ -0,0 +1,28 @@
+/*
+  Equipe: 
+        João Vitor da Conceição Silva I - Subturma A (Líder) 
+        Iago Esron Santiago de Amarante II - Subturma A 
+        Etapa 1
+*/
+
+var x=100, y=100;
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(20);
+  if(keyIsDown(LEFT_ARROW))
+     x-=10
+
+  if(keyIsDown(RIGHT_ARROW))
+    x+=10
+  
+  if(keyIsDown(UP_ARROW))
+    y-=10
+  
+  if(keyIsDown(DOWN_ARROW))
+    y+=10
+  
+  ellipse(x,y,20,20)
+}
