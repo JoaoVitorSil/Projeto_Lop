5d0d6d19e8d69085458193b433183a302b588daf
JoaoVitorSil <48871120+JoaoVitorSil@users.noreply.github.com>
2019-05-10 21:37:44 -0300

    Update Etapa2.js

--- Etapa2.js
+++ Etapa2.js
@@ -9,20 +9,20 @@ var x=100, y=100;
 function setup() {
   createCanvas(600, 400);
 }
-
+ellipse(x,y,20,20)
 function draw() {
   background(20);
+  
   if(keyIsDown(LEFT_ARROW)){
     x-=10
-}
+  }
   if(keyIsDown(RIGHT_ARROW)){
     x+=10
-}
+  }
   if(keyIsDown(UP_ARROW)){
     y-=10
-}  
+  }  
   if(keyIsDown(DOWN_ARROW))
     y+=10
-}
-  ellipse(x,y,20,20)
+  }
 }
